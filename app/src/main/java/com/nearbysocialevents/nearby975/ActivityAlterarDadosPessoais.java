39747a3cf5a6bb4a27df3d2650a7984901327c41
root <humbertopoliti@gmail.com>
2016-06-25 13:35:08 -0700

    criacao de evento finalizada

--- app/src/main/java/com/nearbysocialevents/nearby975/ActivityAlterarDadosPessoais.java
+++ app/src/main/java/com/nearbysocialevents/nearby975/ActivityAlterarDadosPessoais.java
@@ -3,10 +3,28 @@ package com.nearbysocialevents.nearby975;
 import android.app.Activity;
 import android.os.Bundle;
 
+
+
 /**
  * Created by root on 6/25/16.
  */
 public class ActivityAlterarDadosPessoais extends Activity {
+
+
+
+
+
+
+    /**
+     * Decide se os dados no formulario sao validos ou nao
+     * @return true se dados validos ou false se dados invalidos
+     */
+    private boolean isFormDataValid(){
+
+
+        return true;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
