6b5f5e9cf4c0df9f2f90fb2ba36551b19974c806
root <humbertopoliti@gmail.com>
2016-06-29 13:11:05 -0700

    add update sql

---
+++ app/src/main/java/com/nearbysocialevents/nearby975/MySql/UpdateMySql.java
@@ -0,0 +1,55 @@
+package com.nearbysocialevents.nearby975.MySql;
+
+import android.os.AsyncTask;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import android.util.Log;
+
+import com.nearbysocialevents.nearby975.MySql.dbMySQL;
+
+/**
+ * Created by BalaPC on 03-Jun-16.
+ *
+ SendfeedbackJob job = new SendfeedbackJob();
+ job.execute();
+ dbMySQL dbmysql = new dbMySQL();
+ dbmysql.conectarMySQL("127.0.0.1", "3306", "test", "root", "bala123"); // ip do servidor mysql, porta, banco, usuário, senha
+ dbmysql.queryMySQL();
+ dbmysql.desconectarMySQL();
+ *
+ */
+
+
+
+public class UpdateMySql extends AsyncTask<String, Void, Integer> {
+    /** The system calls this to perform work in a worker thread and
+     * delivers it the parameters given to AsyncTask.execute() */
+    private dbMySQL dbmysql = new dbMySQL();
+    private TabelaFuncMySql tmpTabela = new TabelaFuncMySql();
+    private int escolhaExecucao = 1;
+    //private ClassTmpUsar tmpUsar;
+    private int indiceTabela;
+
+    protected Integer doInBackground(String... sql) {
+        dbmysql.conectarMySQL(); // ip do servidor mysql, porta, banco, usuário, senha
+        dbmysql.updatesMySQL(sql[0]);
+        return 1;
+    }
+
+    /** The system calls this to perform work in the UI thread and delivers
+     * the result from doInBackground() */
+    protected void onPostExecute(Integer result) {
+        try {
+
+
+        }catch (Exception erro){
+            Log.e("MYSQL","Erro: "+erro);
+        }
+        dbmysql.desconectarMySQL();
+    }
+
+
+
+
+}
